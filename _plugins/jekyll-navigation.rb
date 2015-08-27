a08825ade3f7dbe69b8ad2a8ceafc1f6dbea1bb1
Julian Hartline <julianh2o@gmail.com>
2015-08-27 12:23:27 -0700

    initial commit

---
+++ _plugins/jekyll-navigation.rb
@@ -0,0 +1,38 @@
+require_relative "jekyll-navigation/version"
+require_relative "jekyll-navigation/navigation_item"
+require_relative "jekyll-navigation/navigation"
+require "liquid"
+
+module JekyllNavigation
+  class NavigationControllerTag < Liquid::Block
+    def initialize(tag_name, level, tokens)
+      super
+      @level = level.to_s.strip
+    end
+
+    def render(context)
+      navigation = Navigation.new(context, @level)
+      
+      if navigation.display?
+        super
+      else
+        ""
+      end
+    end
+  end
+
+  class NavigationTag < Liquid::Tag
+    def initialize(tag_name, level, tokens)
+      super
+      @level = level.to_s.strip
+    end
+
+    def render(context)
+      navigation = Navigation.new(context, @level)
+      navigation.render
+    end
+  end
+
+  Liquid::Template.register_tag('if_navigation', NavigationControllerTag)
+  Liquid::Template.register_tag('navigation', NavigationTag)
+end
---
+++ _posts/2015-8-27-flickerstrip.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "Flickerstrip"
+description: Flickerstrip.com is live, subscribe to keep tabs on our progress!
+author: julian
+categories: news
+---
+
+<!--more-->
