4e7b4564611916636d85c15dd98c974c8a7c6454
Julian Hartline <julianh2o@gmail.com>
2016-04-10 15:06:36 -0700

    fixing require js loading bug

---
+++ lightwork/build.js
@@ -0,0 +1,20 @@
+({
+    baseUrl: ".",
+    "shim": {
+        "bootstrap" : { "deps" :['jquery'] },
+        "jquery.spectrum"  : { "deps" :["jquery"] },
+    },
+    paths: {
+        "view":"./view",
+        "tmpl":"./view/tmpl",
+        "jquery":"./view/lib/jquery",
+        "underscore":"./view/lib/underscore",
+        "tinycolor":"./view/lib/tinycolor",
+        "text":"./view/lib/text",
+        "jquery.spectrum":"./view/lib/jquery.spectrum",
+        "bootstrap":"./view/lib/bootstrap.min",
+    },
+    include: ['./view/lib/require.js'],
+    name: "main",
+    out: "main-built.js"
+})
